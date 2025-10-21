e5fd20d06fe1336a95b801af18ea4616a1a0885e
pbandj082 <pbandj082@gmail.com>
2025-10-21 23:13:13 +0900

    chore: re-export domain in appletheia crate

--- crates/appletheia/src/lib.rs
+++ crates/appletheia/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use appletheia_domain::{
+    aggregate,
+    event,
+    identifier,
+    repository,
+    snapshot,
+    timestamp,
+    value_object,
+};
