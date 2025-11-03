09eed8042e66796385e4d25a60714d5a54a92571
pbandj082 <pbandj082@gmail.com>
2025-11-03 22:08:55 +0900

    wip

--- crates/appletheia-application/src/lib.rs
+++ crates/appletheia-application/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod event;
+pub mod unit_of_work;
 
 pub use event::*;
+pub use unit_of_work::*;
---
+++ crates/appletheia-application/src/unit_of_work.rs
@@ -0,0 +1,5 @@
+pub mod unit_of_work_error;
+
+pub use unit_of_work_error::UnitOfWorkError;
+
+pub trait UnitOfWork {}
