324823b86652a237d487e69432bf2d71a76e9ace
pbandj082 <pbandj082@gmail.com>
2025-11-13 01:12:32 +0900

    chore: start outbox relay

--- crates/appletheia-application/src/lib.rs
+++ crates/appletheia-application/src/lib.rs
@@ -1,5 +1,7 @@
+pub mod outbox;
 pub mod request_context;
 pub mod unit_of_work;
 
+pub use outbox::*;
 pub use request_context::*;
 pub use unit_of_work::*;
---
+++ crates/appletheia-application/src/outbox/outbox_relay.rs
@@ -0,0 +1,2 @@
+#[allow(async_fn_in_trait)]
+pub trait OutboxRelay {}
