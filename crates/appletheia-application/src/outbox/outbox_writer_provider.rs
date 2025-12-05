e866c9885981fde24552dc7933e35777e25c44e1
pbandj082 <51738437+pbandj082@users.noreply.github.com>
2025-12-05 14:18:12 +0900

    Restore infallible publisher access

--- crates/appletheia-application/src/outbox/outbox_writer_provider.rs
+++
@@ -1,9 +0,0 @@
-use super::OutboxWriter;
-
-pub trait OutboxWriterProvider {
-    type OutboxWriter<'c>: OutboxWriter
-    where
-        Self: 'c;
-
-    fn outbox_writer(&mut self) -> Self::OutboxWriter<'_>;
-}
