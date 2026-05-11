b9c4f0361754043b48fdc41585268a1d0b1e0c35
pbandj082 <pbandj082@gmail.com>
2026-05-11 11:10:39 +0900

    refactor(application): adjust checksum algorithm string conversion

--- crates/appletheia-application/src/object_storage/object_checksum_algorithm.rs
+++ crates/appletheia-application/src/object_storage/object_checksum_algorithm.rs
@@ -2,7 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-#[non_exhaustive]
 pub enum ObjectChecksumAlgorithm {
     Md5,
     Crc32c,
@@ -10,11 +9,17 @@ pub enum ObjectChecksumAlgorithm {
 }
 
 impl ObjectChecksumAlgorithm {
-    pub fn as_str(&self) -> &'static str {
+    pub fn as_str(self) -> &'static str {
         match self {
             Self::Md5 => "md5",
             Self::Crc32c => "crc32c",
             Self::Sha256 => "sha256",
         }
+    }
+}
+
+impl AsRef<str> for ObjectChecksumAlgorithm {
+    fn as_ref(&self) -> &str {
+        self.as_str()
     }
 }
