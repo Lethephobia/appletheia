dd608524fc219d3ced79c8d4b11f8650f8d918da
pbandj082 <pbandj082@gmail.com>
2026-07-14 03:28:03 +0900

    refactor(onchain): minimize deposit receipt state

--- examples/banking/onchain/programs/banking-ledger/src/account/pool_token_deposit_receipt.rs
+++ examples/banking/onchain/programs/banking-ledger/src/account/pool_token_deposit_receipt.rs
@@ -7,8 +7,6 @@ pub use pool_token_deposit_receipt_initialization::PoolTokenDepositReceiptInitia
 #[account]
 pub struct PoolTokenDepositReceipt {
     pub version: u8,
-    pub mint_id: [u8; 16],
-    pub token_account_owner: Pubkey,
     pub amount: u64,
     pub bump: u8,
 }
@@ -16,19 +14,12 @@ pub struct PoolTokenDepositReceipt {
 impl PoolTokenDepositReceipt {
     pub const SEED: &[u8] = b"pool_token_deposit_receipt";
     pub const VERSION: u8 = 1;
-    pub const LEN: usize = 1 + 16 + 32 + 8 + 1;
+    pub const LEN: usize = 1 + 8 + 1;
 
     pub fn initialize(&mut self, initialization: PoolTokenDepositReceiptInitialization) {
-        let PoolTokenDepositReceiptInitialization {
-            mint_id,
-            token_account_owner,
-            amount,
-            bump,
-        } = initialization;
+        let PoolTokenDepositReceiptInitialization { amount, bump } = initialization;
 
         self.version = Self::VERSION;
-        self.mint_id = mint_id;
-        self.token_account_owner = token_account_owner;
         self.amount = amount;
         self.bump = bump;
     }
--- examples/banking/onchain/programs/banking-ledger/src/account/pool_token_deposit_receipt/pool_token_deposit_receipt_initialization.rs
+++ examples/banking/onchain/programs/banking-ledger/src/account/pool_token_deposit_receipt/pool_token_deposit_receipt_initialization.rs
@@ -1,8 +1,4 @@
-use anchor_lang::prelude::*;
-
 pub struct PoolTokenDepositReceiptInitialization {
-    pub mint_id: [u8; 16],
-    pub token_account_owner: Pubkey,
     pub amount: u64,
     pub bump: u8,
 }
