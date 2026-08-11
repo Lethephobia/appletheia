73439949f1f4a3325430296c8e404f788df89377
pbandj082 <pbandj082@gmail.com>
2026-08-11 23:33:34 +0900

    refactor(command)!: remove single-command outbox enqueue

--- crates/appletheia-application/src/outbox/command/command_outbox_enqueuer.rs
+++ crates/appletheia-application/src/outbox/command/command_outbox_enqueuer.rs
@@ -7,15 +7,7 @@ use super::CommandOutboxEnqueueError;
 pub trait CommandOutboxEnqueuer: Send + Sync {
     type Uow: UnitOfWork;
 
-    async fn enqueue_command(
-        &self,
-        uow: &mut Self::Uow,
-        command: &CommandEnvelope,
-    ) -> Result<(), CommandOutboxEnqueueError> {
-        self.enqueue_commands(uow, std::slice::from_ref(command))
-            .await
-    }
-
+    /// Enqueues one or more commands atomically.
     async fn enqueue_commands(
         &self,
         uow: &mut Self::Uow,
